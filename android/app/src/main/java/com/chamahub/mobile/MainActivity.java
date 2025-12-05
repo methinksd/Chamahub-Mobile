508e152f5ed8e3e41e150f38c19d4e61073e839f
methinksd <leo.chege01@gmail.com>
2025-12-05 13:31:05 +0300

    Initial commit: Ionic Angular project with Capacitor and Android platform

---
+++ android/app/src/main/java/com/chamahub/mobile/MainActivity.java
@@ -0,0 +1,5 @@
+package com.chamahub.mobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chamahub.mobile',
+  appName: 'Chamahub Mobile',
+  webDir: 'www'
+};
+
+export default config;
